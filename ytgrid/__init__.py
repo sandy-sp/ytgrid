97a08634ac0a0de096104ee4cce1af52607f577f
sandy-sp <sandeep.paidipati@gmail.com>
2025-02-10 18:55:45 -0500

    Update .gitignore, MANIFEST.in, and pyproject.toml; bump version to 2.0.0

--- ytgrid/__init__.py
+++ ytgrid/__init__.py
@@ -1,3 +1,3 @@
 """YTGrid: YT Automation Library"""
-__version__ = "0.1.0"
+__version__ = "2.0.0"
 
--- ytgrid/automation/__init__.py
+++ ytgrid/automation/__init__.py
@@ -1,5 +1,3 @@
-# ytgrid/automation/__init__.py
-
 from .base_player import AutomationPlayer
 from .player import VideoPlayer, play_video
 from .browser import get_browser
